506f31c212072632e48524dc334b54533d1a73ed
Shubham Mapara <mapara.shubham@gmail.com>
2020-04-14 22:01:32 +0530

    Updates

---
+++ precache-manifest.79ba42b1980a064e5ea6b21faae23370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e301ad1fb143a090985318c9a47fadf7",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "34ca6f9b146556a848c5",
+    "url": "/myportfolio/static/css/main.0750f7d4.chunk.css"
+  },
+  {
+    "revision": "6912bf3dc55ed4f6eab3",
+    "url": "/myportfolio/static/js/2.531859b3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myportfolio/static/js/2.531859b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ca6f9b146556a848c5",
+    "url": "/myportfolio/static/js/main.0b991305.chunk.js"
+  },
+  {
+    "revision": "7bbbb5caba61bd14491c",
+    "url": "/myportfolio/static/js/runtime-main.cb8afcd2.js"
+  },
+  {
+    "revision": "b0757ff963c533cbd929d0b1256ca527",
+    "url": "/myportfolio/static/media/profile.b0757ff9.jpg"
+  }
+]);
